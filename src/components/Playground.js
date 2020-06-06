5686654a8fe851f1398f68ff12e8cffccfa3a331
kolpakovin <55667539+kolpakovin@users.noreply.github.com>
2020-06-06 11:16:01 +0300

    random color for border

--- src/components/Playground.js
+++ src/components/Playground.js
@@ -1,10 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import randomColor from "randomcolor";
 
 const Playground = () => {
     const [count, setCount] = useState(0);
+
+    const [color, setColor] = useState(null);
+    useEffect(() => {
+        setColor(randomColor())
+    }, [count])
     return (
-        <div>
+        <div style={{ borderTop: `10px solid ${color}` }}>
             {count}
             <button onClick={() => setCount(currentCount => currentCount - 1)}>
                 -
