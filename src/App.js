6df1aee272d8d247d2d81bc11aa1c7864a771394
kolpakovin <55667539+kolpakovin@users.noreply.github.com>
2020-06-06 10:51:12 +0300

    playground component was added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Playground from './components/Playground'
+
 function App() {
   return (
     <div className="App">
-      
+      <Playground />
     </div>
   );
 }
---
+++ src/components/Playground.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import randomColor from "randomcolor";
+
+const Playground = () => {
+    const [count, setCount] = useState(0);
+    return (
+        <div>
+            {count}
+            <button onClick={() => setCount(currentCount => currentCount - 1)}>
+                -
+            </button>
+            <button onClick={() => setCount(currentCount => currentCount + 1)}>
+                +
+            </button>
+        </div>
+    )
+}
+
+export default Playground;
